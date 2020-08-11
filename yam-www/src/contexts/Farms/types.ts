95335573439c4a78d9f685f04b022d5bc0dbce71
clintonbembryjr <clintonbembryjr@gmail.com>
2020-08-10 21:29:07 -0700

    token addresses

--- yam-www/src/contexts/Farms/types.ts
+++ yam-www/src/contexts/Farms/types.ts
@@ -1,7 +1,9 @@
 export interface Farm {
   name: string,
   depositToken: string,
+  depositTokenAddress: string,
   earnToken: string,
+  earnTokenAddress: string,
   icon: React.ReactNode,
   id: string,
 }
