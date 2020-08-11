cda6c5a872cc5c1352dc29d1c2b8280d713632be
clintonbembryjr <clintonbembryjr@gmail.com>
2020-08-10 23:40:02 -0700

    token balances

---
+++ yam-www/src/constants/tokenAddresses.ts
@@ -0,0 +1,1 @@
+export const yam = '0x0e2298e3b3390e3b945a5456fbf59ecc3f55da16'
---
+++ yam-www/src/hooks/useTokenBalance.ts
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useState } from 'react'
+
+import BigNumber from 'bignumber.js'
+import { useWallet } from 'use-wallet'
+import { provider } from 'web3-core'
+
+import { getBalance } from '../utils/erc20'
+
+const useTokenBalance = (tokenAddress: string) => {
+  const [balance, setBalance] = useState(new BigNumber(0))
+  const { account, ethereum }: { account: string, ethereum: provider } = useWallet()
+
+  const fetchBalance = useCallback(async () => {
+    const balance = await getBalance(ethereum, tokenAddress, account)
+    setBalance(new BigNumber(balance))
+  }, [account, ethereum, tokenAddress])
+
+  useEffect(() => {
+    if (account && ethereum) {
+      fetchBalance()
+    }
+  }, [account, ethereum, setBalance])
+
+  return balance
+}
+
+export default useTokenBalance
