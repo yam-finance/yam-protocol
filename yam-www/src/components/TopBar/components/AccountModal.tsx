6d6e7e95eb38b10c99b82ac24df1fbd1c3d40c3e
Lewis Freiberg <freiberg.lewis@gmail.com>
2020-08-15 23:54:08 +0200

    make SignOut button functional

--- yam-www/src/components/TopBar/components/AccountModal.tsx
+++ yam-www/src/components/TopBar/components/AccountModal.tsx
@@ -1,6 +1,8 @@
 import React, { useCallback, useMemo } from 'react'
 import styled from 'styled-components'
 
+import { useWallet } from 'use-wallet'
+
 import { yam as yamAddress } from '../../../constants/tokenAddresses'
 import useTokenBalance from '../../../hooks/useTokenBalance'
 import { getDisplayBalance } from '../../../utils/formatBalance'
@@ -14,8 +16,10 @@ import Modal, { ModalProps } from '../../Modal'
 import ModalTitle from '../../ModalTitle'
 
 const AccountModal: React.FC<ModalProps> = ({ onDismiss }) => {
+  const { reset } = useWallet()
 
   const handleSignOutClick = useCallback(() => {
+    reset()
     onDismiss!()
   }, [onDismiss])
 
@@ -46,27 +50,20 @@ const AccountModal: React.FC<ModalProps> = ({ onDismiss }) => {
       </StyledBalanceWrapper>
 
       <StyledSpacer />
-      <Button
-        href=""
-        text="More info"
-        variant="secondary"
-      />
+      <Button href="" text="More info" variant="secondary" />
       <StyledSpacer />
-      <Button
-        onClick={handleSignOutClick}
-        text="Sign out"
-      />
+      <Button onClick={handleSignOutClick} text="Sign out" />
     </Modal>
   )
 }
 
 const StyledSpacer = styled.div`
-  height: ${props => props.theme.spacing[4]}px;
-  width: ${props => props.theme.spacing[4]}px;
+  height: ${(props) => props.theme.spacing[4]}px;
+  width: ${(props) => props.theme.spacing[4]}px;
 `
 
 const StyledValue = styled.div`
-  color: ${props => props.theme.color.grey[600]};
+  color: ${(props) => props.theme.color.grey[600]};
   font-size: 36px;
   font-weight: 700;
 `
@@ -81,18 +78,18 @@ const StyledBalanceWrapper = styled.div`
   align-items: center;
   display: flex;
   flex-direction: column;
-  margin-bottom: ${props => props.theme.spacing[4]}px;
+  margin-bottom: ${(props) => props.theme.spacing[4]}px;
 `
 
 const StyledBalanceIcon = styled.div`
   font-size: 36px;
-  margin-right: ${props => props.theme.spacing[3]}px;
+  margin-right: ${(props) => props.theme.spacing[3]}px;
 `
 
 const StyledBalanceActions = styled.div`
   align-items: center;
   display: flex;
-  margin-top: ${props => props.theme.spacing[4]}px;
+  margin-top: ${(props) => props.theme.spacing[4]}px;
 `
 
 export default AccountModal
