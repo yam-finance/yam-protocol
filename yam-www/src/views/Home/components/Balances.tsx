1a1cee5ef5327765645aedf727190f098f5e597b
clintonbembryjr <clintonbembryjr@gmail.com>
2020-08-19 13:22:12 -0700

    unharvested balance

--- yam-www/src/views/Home/components/Balances.tsx
+++ yam-www/src/views/Home/components/Balances.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import numeral from 'numeral'
+import { useWallet } from 'use-wallet'
 
 import Card from '../../../components/Card'
 import CardContent from '../../../components/CardContent'
@@ -12,13 +13,38 @@ import YamIcon from '../../../components/YamIcon'
 
 import { yam as yamAddress, yamv2 as yamV2Address } from '../../../constants/tokenAddresses'
 
+import { Farm } from '../../../contexts/Farms'
+
+import useFarms from '../../../hooks/useFarms'
 import useTokenBalance from '../../../hooks/useTokenBalance'
+import useYam from '../../../hooks/useYam'
 
 import { bnToDec } from '../../../utils'
+import { getEarned } from '../../../yamUtils'
 
 const Balances: React.FC = () => {
+  const [farms] = useFarms()
   const v1Balance = useTokenBalance(yamAddress)
   const v2Balance = useTokenBalance(yamV2Address)
+  const [unharvestedBalance, setUnharvestedBalance] = useState(0)
+
+  const { account } = useWallet()
+  const yam = useYam()
+
+  useEffect(() => {
+    async function fetchUnharvested () {
+      const unharvestedBalances = await Promise.all(farms.map(async (farm: Farm) => {
+        const earnings = await getEarned(yam, farm.contract, account)
+        return bnToDec(earnings)
+      }))
+      const totalBal = unharvestedBalances.reduce((acc, val) => acc + val)
+      setUnharvestedBalance(totalBal)
+    }
+    if (account && farms.length && yam) {
+      fetchUnharvested()
+    }
+  }, [account, farms, setUnharvestedBalance, yam])
+
   return (
     <Card>
       <CardContent>
@@ -46,10 +72,20 @@ const Balances: React.FC = () => {
             </div>
           </StyledBalance>
         </StyledBalances>
+        <Spacer />
+        {!!unharvestedBalance && (
+          <StyledUnharvestedWarning>You have {unharvestedBalance.toLocaleString()} unharvested YAMs</StyledUnharvestedWarning>
+        )}
       </CardContent>
     </Card>
   )
 }
+
+const StyledUnharvestedWarning = styled.div`
+  color: ${props => props.theme.color.secondary.main};
+  font-size: 12px;
+  text-align: center;
+`
 
 const StyledBalances = styled.div`
   display: flex;
