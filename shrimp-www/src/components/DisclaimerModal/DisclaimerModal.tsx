9c25caf22431d416b1c33acb9f229571383efd07
Superdcc <superdcc@progressing.today>
2020-08-25 14:14:26 +0800

    before invest warning

--- shrimp-www/src/components/DisclaimerModal/DisclaimerModal.tsx
+++ shrimp-www/src/components/DisclaimerModal/DisclaimerModal.tsx
@@ -26,6 +26,8 @@ const DisclaimerModal: React.FC<DisclaimerModal> = ({ onConfirm, onDismiss }) =>
           <p>Audits: None. (This project is in beta. Use at your own risk.)</p>
           <p>🦐 New Advanced Pool will start on 2020/08/26 12:00:00 UTC+0, welcome <a href="https://zombie.finance" target="_blank">Zombie.Finance</a> on board. 🦐</p>
           <p>🚨 For more information on the inflation of Uniswap LP pool tokens, please visit <a href="https://github.com/shrimp-finance/shrimp-protocol/wiki/Total-Shrimp:-229,240.92-%F0%9F%A6%90" target="_blank">Github Wiki</a></p>
+          <br/>
+          <p>💡 Any token in Advanced Pool does not constitute an investment opinion, Shrimp Group is only responsible for basic quick check! Please evaluate yourself carefully before buying!</p>
         </div>
       )
     } else {
